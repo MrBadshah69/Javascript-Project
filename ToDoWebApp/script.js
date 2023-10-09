7fba5ef55134789e6cca2e6d895ef66b263d2b51
MrBadshah69 <arsalanakbarali@gmail.com>
2023-10-09 06:15:18 +0500

    ToDo Web App Complete

--- ToDoWebApp/script.js
+++ ToDoWebApp/script.js
@@ -1,13 +1,14 @@
 add = document.getElementById("add");
-
-add.addEventListener("click", (e) => {
+add.addEventListener("click", dataadding);
+datashowing();
+function dataadding() {
     console.log("Its working..............");
 
     tilte = document.getElementById("title").value;
     descri = document.getElementById("descri").value;
     date = document.getElementById("date").value;
 
-    if ((tilte && descri && date) == "") {
+    if ((tilte || descri || date) == "") {
         alert("please Fill This Boxes");
     } else {
 
@@ -31,32 +32,73 @@ add.addEventListener("click", (e) => {
         }
 
 
-        tablebody = document.getElementById("tableBody");
+        datashowing();
+    }
+}
 
-        data = "";
+function datashowing() {
+    if (localStorage.getItem("dataJson") == null) {
 
-        dataarrayjson.forEach((element , index) => {
+        dataarrayjson = [];
 
-            data += `
+        localStorage.setItem("dataJson", JSON.stringify(dataarrayjson));
+    } else {
+
+        dataarrayjsonstr = localStorage.getItem("dataJson");
+
+
+        dataarrayjson = JSON.parse(dataarrayjsonstr);
+
+    }
+
+    tablebody = document.getElementById("tableBody");
+
+    data = "";
+
+    dataarrayjson.forEach((element, index) => {
+
+        data += `
             <tr>
               <th scope="row">${index + 1}</th>
               <td>${element[0]}</td>
               <td>${element[1]}</td>
               <td>${element[2]}</td>
-              <td>Delete</td>
+              <td><button class="btn btn-danger" type="button" onclick="deleted(${index})" >Delete</button>
+              </td>
             </tr>
             `
 
 
-        });
+    });
 
 
 
 
-        tablebody.innerHTML = data;
+    tablebody.innerHTML = data;
 
 
 
+}
+
+function deleted(datad) {
+
+    dataarrayjsonstr = localStorage.getItem("dataJson");
+
+
+    dataarrayjson = JSON.parse(dataarrayjsonstr);
+    dataarrayjson.splice(datad, 1);
+
+    localStorage.setItem("dataJson", JSON.stringify(dataarrayjson));
+
+    datashowing();
+}
+
+function ClearAll() {
+
+    if (confirm("Are your sure your data is permently delete")) {
+
+        localStorage.clear()
+
+        dataadding()
     }
-
-});
+}
